e667e1be6e666e5224dd85dd25948e429807422d
taeksoolee <leets1490@gmail.com>
2023-05-03 00:21:20 +0900

    style: 스타일 작업

--- client/my-bbs/tailwind.config.js
+++ client/my-bbs/tailwind.config.js
@@ -13,6 +13,24 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: require('tailwindcss/colors').white,
+      black: require('tailwindcss/colors').black,
+      light: require('tailwindcss/colors').slate[100],
+      dark: require('tailwindcss/colors').slate[700],
+      primary: {
+        ...require('tailwindcss/colors').slate,
+        active: require('tailwindcss/colors').slate[500],
+      },
+      // {
+      //   DEFAULT: '#F00',
+      //   100: '#F10',
+      // },
+      secondary: require('tailwindcss/colors').indigo,
+      danger: require('tailwindcss/colors').red,
+    }
   },
   plugins: [],
 }
