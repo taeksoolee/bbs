dd7c35a9219338427ef3371ddc5b9f28a406948a
taeksoolee <leets1490@gmail.com>
2023-05-06 23:07:23 +0900

    test: api Route 테스트

---
+++ client/my-bbs/src/app/api/contents/[id]/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request, context: {
+  params: { id: string }
+}) {
+  console.log(context.params.id);
+  console.log(request.url);
+  return NextResponse.json([6, 'AAABBBCCC', '2023-05-04']);
+}
--- client/my-bbs/src/app/api/contents/route.ts
+++ client/my-bbs/src/app/api/contents/route.ts
@@ -1,12 +1,19 @@
+// https://nextjs.org/docs/app/building-your-application/routing/router-handlers
+
+import { NextResponse } from 'next/server';
+
 export async function GET(request: Request) {
-  return new Response(
-    JSON.stringify([
-      [6, 'AAABBBCCC', '2023-05-04'],
-      [5, 'Apple Color is Red', '2023-05-02'],
-      [4, 'Banana Color is Yellow', '2023-05-02'],
-      [3, 'Sky Color is Blue', '2023-05-02'],
-      [2, 'Stone Color is Slate', '2023-05-02'],
-      [1, 'Dark Color is Black', '2023-05-02'],
-    ]),
-  );
+  return NextResponse.json([
+    [6, 'AAABBBCCC', '2023-05-04'],
+    [5, 'Apple Color is Red', '2023-05-02'],
+    [4, 'Banana Color is Yellow', '2023-05-02'],
+    [3, 'Sky Color is Blue', '2023-05-02'],
+    [2, 'Stone Color is Slate', '2023-05-02'],
+    [1, 'Dark Color is Black', '2023-05-02'],
+  ]);
+}
+
+export async function POST(request: Request) {
+  console.log('post!!');
+  return new Response();
 }
